d13fb35c947f65c546c09c8c0caa9814b76b21a8
Abhijeet Pahade <abhip99@gmail.com>
2024-06-19 16:56:44 +0530

    dark theme implement

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import Header from "../components/Header";
 import "./globals.css";
+import Providers from "./Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,8 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}</body>
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
